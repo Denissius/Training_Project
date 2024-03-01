26f4b3c8ead9dec22fb0482cfbc02bd42047325d
Denissius <denissius1987@gmail.com>
2024-03-01 19:34:34 +0300

    Начало построения доменной модели

--- _Training_project/Service/Config.cs
+++ _Training_project/Service/Config.cs
@@ -7,5 +7,7 @@ namespace _Training_project.Service
 {
     public class Config
     {
+        public static string ConnectionString { get; set; }
+        public static string MainPage { get; set; }
     }
 }
--- _Training_project/Startup.cs
+++ _Training_project/Startup.cs
@@ -1,6 +1,8 @@
+using _Training_project.Service;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -12,9 +14,12 @@ namespace _Training_project
 {
     public class Startup
     {
-        
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration) => Configuration = configuration;
+
         public void ConfigureServices(IServiceCollection services)
         {
+            Configuration.Bind("Project", new Config());
             services.AddControllersWithViews();
         }
 
